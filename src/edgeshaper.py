8a874d1bb63291ed58ded4737a7df39efbe38cf0
Andrea Mastropietro <andrea.mastropietro@gmail.com>
2022-06-20 15:20:21 +0200

    Updating edgeshaper.py

--- src/edgeshaper.py
+++ src/edgeshaper.py
@@ -9,7 +9,7 @@ from numpy.random import default_rng
 from tqdm import tqdm
 
 
-def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, deviation = None, log_odds = False, seed = 42):
+def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, deviation = None, log_odds = False, seed = 42, device = "cpu"):
     """ Compute Shapley values approximation for edge importance in GNNs
         Args:
             model (Torch.NN.Module): Torch GNN model used.
@@ -24,21 +24,22 @@ def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, devi
                 the procedure always terminates.
             log_odds (bool, optional). Default is ```False```. If ```True```, use log odds instead of probabilty as Value for Shaply values approximation.
             seed (float, optional): seed used for the random number generator.
+            device (string, optional): states if using ```cuda``` or ```cpu```.
         Returns:
                 list: list of Shapley values for the edges computed by EdgeSHAPer. The order of the edges is the same as in ```E```.
         """
     
     if deviation != None:
-        return edgeshaper_deviation()
+        return edgeshaper_deviation(model, x, E, num_nodes, M = M, target_class = target_class, P = P, deviation = deviation, log_odds = log_odds, seed = seed, device=device)
 
 
     rng = default_rng(seed = seed)
     model.eval()
     phi_edges = []
 
+    num_edges = E.shape[1]
     if P == None:
         max_num_edges = num_nodes*(num_nodes-1)
-        num_edges = E.shape[1]
         graph_density = num_edges/max_num_edges
         P = graph_density
 
@@ -46,8 +47,6 @@ def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, devi
         marginal_contrib = 0
         for i in range(M):
             E_z_mask = rng.binomial(1, P, num_edges)
-            #E_z_index = torch.IntTensor(torch.nonzero(torch.IntTensor(E_z_mask)).tolist()).to(device).squeeze()
-            #E_z = torch.index_select(E, dim = 1, index = E_z_index)
             E_mask = torch.ones(num_edges)
             pi = torch.randperm(num_edges)
 
@@ -83,7 +82,7 @@ def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, devi
             else:
                 out_prob = out #out prob variable now containts log_odds
             
-            V_j_plus = out_prob[0][target_class].item() #probably the predicted class changes when selecting/deselecing certain edges for class 1: more iterations needed?
+            V_j_plus = out_prob[0][target_class].item()
 
             # without edge j
             retained_indices_minus = torch.LongTensor(torch.nonzero(E_j_minus_index).tolist()).to(device).squeeze()
@@ -106,5 +105,86 @@ def edgeshaper(model, x, E, num_nodes, M = 100, target_class = 0, P = None, devi
     return phi_edges
 
 
-def edgeshaper_deviation():
-    return None
+def edgeshaper_deviation(model, x, E, num_nodes, M = 100, target_class = 0, P = None, deviation = None, log_odds = False, seed = 42, device = "cpu"):
+
+    rng = default_rng(seed = seed)
+    model.eval()
+    batch = torch.zeros(x.shape[0], dtype=int, device=x.device)
+    out = model(x, E, batch=batch)
+    out_prob_real = F.softmax(out, dim = 1)[0][target_class].item()
+
+    phi_edges = []
+    phi_edges_current = [0] * num_edges
+    num_edges = E.shape[1]
+
+    if P == None:
+        max_num_edges = num_nodes*(num_nodes-1)
+        graph_density = num_edges/max_num_edges
+        P = graph_density
+
+    for i in tqdm(range(M)):
+    
+        for j in range(num_edges):
+            
+            
+            E_z_mask = rng.binomial(1, P, num_edges)
+            E_mask = torch.ones(num_edges)
+            pi = torch.randperm(num_edges)
+
+            E_j_plus_index = torch.ones(num_edges, dtype=torch.int)
+            E_j_minus_index = torch.ones(num_edges, dtype=torch.int)
+            selected_edge_index = np.where(pi == j)[0].item()
+            for k in range(num_edges):
+                if k <= selected_edge_index:
+                    E_j_plus_index[pi[k]] = E_mask[pi[k]]
+                else:
+                    E_j_plus_index[pi[k]] = E_z_mask[pi[k]]
+
+            for k in range(num_edges):
+                if k < selected_edge_index:
+                    E_j_minus_index[pi[k]] = E_mask[pi[k]]
+                else:
+                    E_j_minus_index[pi[k]] = E_z_mask[pi[k]]
+
+
+            #we compute marginal contribs
+            
+            # with edge j
+            retained_indices_plus = torch.LongTensor(torch.nonzero(E_j_plus_index).tolist()).to(device).squeeze()
+            E_j_plus = torch.index_select(E, dim = 1, index = retained_indices_plus)
+
+            batch = torch.zeros(x.shape[0], dtype=int, device=x.device)
+            
+            out = model(x, E_j_plus, batch=batch)
+            out_prob = None
+
+            if not log_odds:
+                out_prob = F.softmax(out, dim = 1)
+            else:
+                out_prob = out #out prob variable now containts log_odds
+            
+            V_j_plus = out_prob[0][target_class].item()
+
+            # without edge j
+            retained_indices_minus = torch.LongTensor(torch.nonzero(E_j_minus_index).tolist()).to(device).squeeze()
+            E_j_minus = torch.index_select(E, dim = 1, index = retained_indices_minus)
+
+            batch = torch.zeros(x.shape[0], dtype=int, device=x.device)
+            out = model(x, E_j_minus, batch=batch)
+
+            if not log_odds:
+                out_prob = F.softmax(out, dim = 1)
+            else:
+                out_prob = out
+            
+            V_j_minus = out_prob[0][target_class].item()
+
+            phi_edges_current[j] += (V_j_plus - V_j_minus)
+
+        
+        phi_edges = [elem / (i+1) for elem in phi_edges_current]
+        print(sum(phi_edges))
+        if abs(out_prob_real - sum(phi_edges)) <= deviation:
+            break
+             
+    return phi_edges
