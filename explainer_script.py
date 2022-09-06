045583e9a27afaf036024b37169eb36ff3a995b3
Andrea Mastropietro <andrea.mastropietro@gmail.com>
2022-09-06 18:00:11 +0200

    Edited explainer script, fixed bugs, reverted torchdrug version and uploaded test results

---
+++ explainer_script.py
@@ -0,0 +1,209 @@
+### run this script to obtain explanations for given molecules using a pretrained model ###
+import argparse
+import os
+
+import torch
+from torch_geometric.data import  Data
+from torchdrug import data
+
+from pysmiles import read_smiles
+
+from tqdm import tqdm
+
+#custom modules
+from src.utils import *
+from src.edgeshaper import *
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def print_usage():
+    print(' ')
+    print('usage: python explainer_script.py --MODEL_PATH --DATA_PATH --MOLECULES_TO_EXPLAIN --TARGET_CLASS --MINIMAL_SETS --SAVE_PATH --SAMPLING_STEPS --VISUALIZATION_SAVE_PATH --TOLERANCE --SEED')
+    print('-----------------------------------------------------------------')
+    print('MODEL_PATH: path in which your model is located.')
+
+    print('DATA_PATH: path in which your .csv dataset file is located.')
+    print('    default: "experiments/data/chembl29_predicting_target_P14416_P42336_target_1_vs_random_cpds.csv."')
+
+    print('MOLECULES_TO_EXPLAIN: path in which your .txt file with the molecules to explain is located.')
+    print('    default: "TBD"')
+
+    print('TARGET_CLASS: target class for which the explanations will be computed.')
+    print('    default: 0')
+
+    print('SMILES_FIELD_NAME :column name for the SMILES field.')
+    print('LABEL_FIELD_NAME :column name for the label field.')
+
+    print('MINIMAL_SETS: boolean indicating whether to compute minimal informative sets.')
+    print('    default: False')
+
+    print('SAVE_PATH: path in which the explanations will be saved.')
+    print('    default: "TBD"')
+
+    print('SAMPLING_STEPS: number of Monte Carlo sampling steps to perform.')
+    print('    default: 100')
+
+    print('VISUALIZATION: if to sve visualizations.')
+    print('    (optional, default: False)')
+
+    print('TOLERANCE: desired deviation between predicted probability and sum of Shapley values.')
+    print('    (optional, default: None')
+
+    print('SEED: seed for the random number generator.')
+    print('    (optional, default: None')
+   
+
+def parse_args():
+    '''
+    Parse the terminal arguments.
+    '''
+    parser = argparse.ArgumentParser(description='Set needed arguments for the explainer script.')
+
+    parser.add_argument('--MODEL_PATH', type=str, required=True,
+            help='path in which your model is located')   
+
+    parser.add_argument('--DATA_PATH', type=str, default="experiments/data/chembl29_predicting_target_P14416_P42336_target_1_vs_random_cpds.csv.",
+                    help='path in which your .csv dataset file is located (default: "experiments/data/chembl29_predicting_target_P14416_P42336_target_1_vs_random_cpds.csv.\\"')
+
+    parser.add_argument('--MOLECULES_TO_EXPLAIN', type=str, required=True, help='path in which your .txt file with the molecules to explain is located (default: "TBD")')
+
+    parser.add_argument('--TARGET_CLASS', type=int, default=0, help='target class for which the explanations will be computed (default: 0)')
+
+    parser.add_argument('--SMILES_FIELD_NAME', type=str, default=None, required=True,
+                help='column name for the SMILES field\\')
+
+    parser.add_argument('--LABEL_FIELD_NAME', type=str, default=None,required=True,
+                help='column name for the label field\\')
+
+    parser.add_argument('--MINIMAL_SETS', type=bool, default=False, help='boolean indicating whether to compute minimal informative sets (default: False)')
+
+    parser.add_argument('--SAVE_PATH', type=str, default="results", help='path in which the explanations will be saved (optional, default: "TBD")')
+
+    parser.add_argument('--SAMPLING_STEPS', type=int, default=100, help='number of Monte Carlo sampling steps to perform (default: 100)')
+
+    parser.add_argument('--VISUALIZATION', type=bool, default=False, help=' if to save visualization (optional, default: False)')
+
+    parser.add_argument('--TOLERANCE', type=float, default=None, help='desired deviation between predicted probability and sum of Shapley values (optional, default: None)')
+
+    parser.add_argument('--SEED', type=int, default=None, help='seed for the random number generator (optional, default: None)')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    # parse arguments
+
+    args = parse_args()
+
+    MODEL_PATH = args.MODEL_PATH
+    DATA_PATH = args.DATA_PATH
+    MOLECULES_TO_EXPLAIN = args.MOLECULES_TO_EXPLAIN
+    TARGET_CLASS = args.TARGET_CLASS
+    SMILES_FIELD_NAME = args.SMILES_FIELD_NAME
+    LABEL_FIELD_NAME = args.LABEL_FIELD_NAME
+    MINIMAL_SETS = args.MINIMAL_SETS
+    SAVE_PATH = args.SAVE_PATH
+    SAMPLING_STEPS = args.SAMPLING_STEPS
+    VISUALIZATION = args.VISUALIZATION
+    TOLERANCE = args.TOLERANCE
+    SEED = args.SEED
+
+    # if not len(sys.argv) > 1:
+    #     print_usage()
+    #     print('ERROR: Not enough arguments provided.')
+    #     sys.exit(1)
+
+    # if MODEL_PATH is None:
+    #     print_usage()
+    #     print('ERROR: No model path provided.')
+    #     sys.exit(1)
+    
+    if SEED is not None:
+        set_all_seeds(SEED)
+
+    # load data
+
+    df_data = load_data(DATA_PATH, SMILES_FIELD_NAME, LABEL_FIELD_NAME)
+    
+    target_fields = [LABEL_FIELD_NAME]
+    chembl_dataset = ChEMBL(path = DATA_PATH, smiles_field = SMILES_FIELD_NAME, target_fields = target_fields)
+
+    #create edge index for each molecule
+    smiles = chembl_dataset.smiles_list
+    mols = []
+    for i in tqdm(range(len(smiles))):
+        mols.append(read_smiles(smiles[i]))
+
+    edge_index_list = []
+    for mol in tqdm(mols):
+        edge_index_list.append(create_edge_index(mol))
+
+    #check if those steps are necessary!!!!### we could get rid of the CHEMbl dataset
+    mols_torchdrug_format = []
+    for i in tqdm(range(len(smiles))):
+        mols_torchdrug_format.append(data.Molecule.from_smiles(smiles[i], with_hydrogen = False))
+
+    #instantiating the dataset
+    data_list = []
+    y = torch.LongTensor(chembl_dataset.targets[LABEL_FIELD_NAME]).to(device)
+
+    for i in tqdm(range(len(mols))):
+        data_list.append(Data(x = mols_torchdrug_format[i].node_feature, edge_index = edge_index_list[i], y = y[i], smiles = chembl_dataset.smiles_list[i]))
+
+    dataset = ChEMBLDatasetPyG(".", data_list = data_list)
+    # load model
+    model = GCN(node_features_dim = chembl_dataset.node_feature_dim, num_classes =dataset.num_classes, hidden_channels=256).to(device)
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.to(device)
+
+    #read list of molecules to explain
+
+    with open(MOLECULES_TO_EXPLAIN, 'r') as f:
+        molecules_to_explain = f.read().splitlines()
+    
+    test_cpd_indices = []
+    for molecule in molecules_to_explain:
+        test_cpd_indices.append(chembl_dataset.smiles_list.index(molecule)) #check if this is correct
+
+    #explain the molecules
+    for test_index in test_cpd_indices:
+        #good idea to define an explainer class, see if we want to implement it
+        # explainer = Explainer(model, dataset, test_index, SAMPLING_STEPS, TOLERANCE)
+        # explanation = explainer.explain()
+        # if MINIMAL_SETS:
+        #     explanation = explainer.compute_minimal_sets(explanation)
+        # if SAVE_PATH is not None:
+        #     explainer.save_explanation(explanation, SAVE_PATH)
+        # if VISUALIZATION_SAVE_PATH is not None:
+        #     explainer.visualize(explanation, VISUALIZATION_SAVE_PATH)
+
+        print("Explaining test compound: ", dataset[test_index].smiles)
+        test_cpd = dataset[test_index].to(device)
+
+        phi_edges = edgeshaper(model, test_cpd.x, test_cpd.edge_index, M = SAMPLING_STEPS, target_class = TARGET_CLASS, P = None, deviation = TOLERANCE, log_odds = False, seed = SEED, device = device)
+        
+        # print("Shapley values for edges: ", phi_edges)
+
+        if SAVE_PATH is not None:
+            SAVE_PATH_COMPLETE = SAVE_PATH + "/"  + test_cpd.smiles
+            if not os.path.exists(SAVE_PATH_COMPLETE):
+                os.makedirs(SAVE_PATH_COMPLETE)
+            INFO_EXPLANATIONS = SAVE_PATH_COMPLETE + "/info_explanations.txt"
+            
+
+            with open(INFO_EXPLANATIONS, "w+") as saveFile:
+                saveFile.write("Explaining class " + str(TARGET_CLASS) + " for compound: " + test_cpd.smiles + "\n")
+
+                saveFile.write("Shapley values for edges:\n")
+                for i in range(len(phi_edges)):
+                    saveFile.write("(" + str(test_cpd.edge_index[0][i]) + "," + str(test_cpd.edge_index[1][i]) + "): " + str(phi_edges[i]) + "\n")
+
+                saveFile.write("\nSum of Shapley values: " + str(sum(phi_edges)) + "\n")
+                
+        if VISUALIZATION:
+            VISUALIZATION_SAVE_PATH_COMPLETE = SAVE_PATH + "/"  + test_cpd.smiles
+
+            if not os.path.exists(VISUALIZATION_SAVE_PATH_COMPLETE):
+                os.makedirs(VISUALIZATION_SAVE_PATH_COMPLETE)
+
+            visualize_explanations(test_cpd, phi_edges, VISUALIZATION_SAVE_PATH_COMPLETE)
--- trainer_script.py
+++ trainer_script.py
@@ -1,29 +1,18 @@
 #import needed libraries
 import argparse
 
-import random
 import sys
-import os
-from datetime import datetime
 
-import pandas as pd
-import numpy as np
 from tqdm import tqdm
-import networkx as nx
 
 import torch
-from torch_geometric.data import InMemoryDataset, Data
+from torch_geometric.data import  Data
 from torch_geometric.loader import DataLoader
-import torch_geometric.transforms as T
-from torch_geometric.nn import GCNConv, Linear
-from torch_geometric.nn import global_mean_pool
-import torch.nn.functional as F
 
 from torchdrug import data
-from torchdrug.core import Registry as R
-from torchdrug.utils import doc
 from pysmiles import read_smiles
 
+from src.utils import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -84,112 +73,6 @@ def parse_args():
 
     return parser.parse_args()
 
-def set_all_seeds(SEED):
-    '''
-    Set all seeds for reproducibility.
-    '''
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed(SEED)
-    torch.backends.cudnn.deterministic = True
-    np.random.seed(SEED)
-    random.seed(SEED)
-
-def load_data(DATA_PATH, SMILES_FIELD_NAME, LABEL_FIELD_NAME):
-    '''
-    Load the data from a .csv file.
-    '''
-    df_data = pd.read_csv(DATA_PATH, sep = ",")
-    df_data = df_data[[SMILES_FIELD_NAME, LABEL_FIELD_NAME]]
-    
-    return df_data
-
-
-def save_model(model, MODEL_SAVE_PATH):
-    '''
-    Save the trained model.
-    '''
-    now = datetime.now()
-    current_time = now.strftime("%Y_%m_%d-%H_%M_%S")
-
-    if not os.path.exists(MODEL_SAVE_PATH):
-        os.makedirs(MODEL_SAVE_PATH)
-
-    MODEL_PATH = MODEL_SAVE_PATH + "/model_" + current_time + ".ckpt"
-    torch.save(model.state_dict(), MODEL_PATH)
-
-def create_edge_index(mol):
-    """
-    Create edge index for a molecule.
-    """
-    adj = nx.to_scipy_sparse_matrix(mol).tocoo()
-    row = torch.from_numpy(adj.row.astype(np.int64)).to(torch.long)
-    col = torch.from_numpy(adj.col.astype(np.int64)).to(torch.long)
-    edge_index = torch.stack([row, col], dim=0)
-    return edge_index
-
-@R.register("datasets.ChEMBL")
-class ChEMBL(data.MoleculeDataset):
-    '''
-    Class for the molecule dataset.
-    '''
-    def __init__(self, path, smiles_field, target_fields, verbose=1, **kwargs):
-    
-        self.path = path
-        self.smiles_field = smiles_field
-        self.target_fields= target_fields
-
-        self.load_csv(self.path, smiles_field=self.smiles_field, target_fields=self.target_fields,
-                    verbose=verbose, **kwargs)
-
-class ChEMBLDatasetPyG(InMemoryDataset):
-    '''
-    Class for the PyG version of the dataset.
-    '''
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None, data_list = None):
-        super().__init__(root, transform, pre_transform, pre_filter)
-        
-        self.data_list = data_list
-
-        data_list = self.data_list
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-
-        self.data, self.slices = self.collate(data_list)
-
-class GCN(torch.nn.Module):
-    '''
-    4-layer GCN model class.
-    '''
-    def __init__(self, hidden_channels):
-        super(GCN, self).__init__()
-        # torch.manual_seed(12345)
-        self.conv1 = GCNConv(chembl_dataset.node_feature_dim, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, hidden_channels)
-        self.conv4 = GCNConv(hidden_channels, hidden_channels)
-        self.lin = Linear(hidden_channels, dataset.num_classes)
-
-    def forward(self, x, edge_index, batch):
-        # 1. Obtain node embeddings 
-        x = self.conv1(x.float(), edge_index)
-        x = x.relu()
-        x = self.conv2(x, edge_index)
-        x = x.relu()
-        x = self.conv3(x, edge_index)
-        x = x.relu()
-        x = self.conv4(x, edge_index)
-        # 2. Readout layer
-        x = global_mean_pool(x, batch)  
-
-        # 3. Apply a final classifier
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin(x)
-        
-        return x
 
 if __name__ == "__main__":
     args = parse_args()
@@ -228,7 +111,7 @@ if __name__ == "__main__":
 
     # Load the dataset
     df_data = load_data(DATA_PATH, SMILES_FIELD_NAME, LABEL_FIELD_NAME)
-
+    
     # instantiate custom class from TorchDrug
     target_fields = [LABEL_FIELD_NAME]
     chembl_dataset = ChEMBL(path = DATA_PATH, smiles_field = SMILES_FIELD_NAME, target_fields = target_fields)
@@ -278,7 +161,7 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_data, batch_size=BATCH_SIZE)
 
     #model instantiation
-    model = GCN(hidden_channels=256).to(device)
+    model = GCN(node_features_dim = chembl_dataset.node_feature_dim, num_classes =dataset.num_classes, hidden_channels=256).to(device)
     
     #training the network
     lr = lr=1e-3
