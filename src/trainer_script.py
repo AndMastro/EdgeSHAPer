d7d7239b8f3cf1e9e2d254f0ff42c0242878ecc5
Andrea Mastropietro <andrea.mastropietro@gmail.com>
2022-09-28 20:55:20 +0200

    Fixing paths

--- src/trainer_script.py
+++ src/trainer_script.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     start = time()
     
     args = None
-    with open("parameters.yml") as paramFile:  
+    with open("../parameters.yml") as paramFile:  
         args = yaml.load(paramFile, Loader=yaml.FullLoader)
 
 
